6a4737c77318e7142b18376b510281b8ea9081f4
Vernik <vernik22.ing@gmail.com>
2021-08-17 17:44:04 -0600

    Creando Proyecto con vs code

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -1,8 +1,30 @@
-#include <QCoreApplication>
+#include "iostream"
+#include "Estructuras/manejador.h"
+#include "Estructuras/Lista/list.h"
+#include "Estructuras/Lista/list.cpp"
+using namespace std;
+void llenarListaDiscos(List<int>);
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    List<int> listaDiscos;
+    llenarListaDiscos(listaDiscos);
+    string p="------------------------------Ingrese un comando------------------------------\n";
 
-    return a.exec();
+    string line;
+
+    while(line!="salir"){    //esto me sirve para seguir leyendo siempre los comandos sin salirme
+        cout << p;
+        getline(cin,line);
+       
+
+    }
+
+
 }
+
+void llenarListaDiscos(List<int> listaDiscos){
+cout<< "Pasa por llenardiscos"<< endl;
+
+}
+
